9f49aa91490f978e3f85dcdfc5f4f52f7d741da6
jhoestevam <jhonatanm222@gmail.com>
2017-08-30 17:34:23 -0300

    initial commit

---
+++ getting-start/task/db/migrate/20170830201214_create_tarefas.rb
@@ -0,0 +1,11 @@
+class CreateTarefas < ActiveRecord::Migration
+  def change
+    create_table :tarefas do |t|
+      t.string :titulo
+      t.text :descricao
+      t.string :status
+
+      t.timestamps null: false
+    end
+  end
+end
